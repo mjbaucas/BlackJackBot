cb935fc42d6a1cbd35cbcfb740815ffcbb2bfec3
Robert Bowen <robbowen@Users-MacBook-Pro-4.local>
2018-03-21 22:38:27 -0400

    Added playing ability to dealer

--- dealer.py
+++ dealer.py
@@ -1,11 +1,38 @@
 from random import *
+from cards import Cards
+
+cards = Cards()
 
 class Dealer:
     def init(self, deck):
         self.deck = deck
+        self.hand = []
+        self.score = 0
 
     def deal_card(self):
         return self.deck.pop()
         
     def shuffle_deck(self):
         shuffle(self.deck)
+
+    def take_card(self, card):
+        self.hand.append(card)
+        self.update_score()
+
+    def update_score(self):
+        score = 0
+        aces = 0
+        for card in self.hand:
+            number, suite = card.split('_')
+            score += cards.scores[number]
+            if number == 'A':
+                aces += 1
+        if score > 21 and aces > 0:
+            while aces > 0 and score > 21:
+                score -= 10
+                aces -= 1
+
+        self.score = score
+
+    def get_score(self):
+        return self.score
--- rules.py
+++ rules.py
@@ -15,3 +15,14 @@ class Rules:
             self.end = 0
             self.new = 0
         return self.end, self.new
+
+    def whowon(self, player_one, dealer):
+        if dealer.get_score() <= 21 & dealer.get_score() > player_one.get_score():
+            # Dealer wins
+            print(f'The dealer wins!')
+        elif player_one.get_score() <= 21 & player_one.get_score() > dealer.get_score():
+            # Player wins
+            print(f'Congratulations! You win!')
+        elif player_one.get_score() == dealer.get_score():
+            # Its a
+            print(f'The result is a push (tie).')
